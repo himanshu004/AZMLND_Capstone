3008052ce64590006ade20ad58a48f08bfdf5c6c
Himanshu Madan <himanshumadan1059@gmail.com>
2021-01-27 13:52:24 +0530

    preproccesed data file

---
+++ train.py
@@ -0,0 +1,45 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+uri = 'https://raw.githubusercontent.com/himanshu004/AZMLND_Capstone/main/dataset.csv'
+ds = TabularDatasetFactory.from_delimited_files(path = uri)
+
+x_df = ds.to_pandas_dataframe().dropna()
+
+y_df = x_df.pop('Exited')
+
+x_train, x_test, y_train,y_test = train_test_split(x_df,y_df,train_size = 0.8,random_state = 42)
+
+run = Run.get_context()
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/hyperDrive_{}_{}'.format(args.C,args.max_iter))
+
+if __name__ == '__main__':
+    main()
